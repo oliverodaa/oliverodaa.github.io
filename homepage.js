81e1efb7cf04625ebe235c7a2e06ed07ceff4152
Ollie O'Donnell <oliverodaa@gmail.com>
2014-08-21 23:51:31 -0700

    Fixed Firefox!
    PROBLEMS:
    1) The Navbar has an extra div so that when it becomes “sticky” the
    page doesn’t resize. Function defining its height wasn’t firing because
    it was a straight function. Apparently firefox doesn’t like that.
    
    2) Two functions depend on window resizing as a trigger. Neither were
    firing. Turns out Firefox wants $(window).resize(function) to be in the
    global frame.
    
    3) Refined the thing that gives the title less padding-top when it
    starts taking up two lines.

--- homepage.js
+++ homepage.js
@@ -1,41 +1,25 @@
-// The point of this function is to make sure that
-// when it changes to fixed position, the page 
-// doesn't have a gap in it
-$(function() {
-	$('#nav-wrapper').height($('#nav').height());
-})
-
 function checkWidth() {
-	var fullWidth = ($('#title_container').css("padding-top") == "70px")
-	if (fullWidth && $('#cover_space').width() < 466) {
+	// This makes sure the title text doesn't look weird on resize
+	var titleLength = $('#title').text().length;
+	var oneLine = ($('#title_container').css("padding-top") == "70px")
+	if (oneLine && $('#cover_space').width() < titleLength*30) {
 		$('#title_container').css({"padding-top":"10px"});
 	}
-	else if (!fullWidth && $('#cover_space').width() > 466) {
+	else if (!oneLine && $('#cover_space').width() >= titleLength*30) {
 		$('#title_container').css({"padding-top":"70px"});
 	}
 }
 
-$(document).ready(function () {
-	var longTitle = ($('#title').text().length > 8);
-	if (longTitle) {
-		checkWidth();
-		$(window).resize(checkWidth);
-	}
-})
-
 function resizeBoard() {
+	// This makes sure that square boards are both square and not too big
 	var spaceWidth = $('#boardDiv').width();
 	var windowHeight = $(window).height();
 	var maxWidth = windowHeight - 100;
 	if (spaceWidth < maxWidth) {
-		// console.log("good size")
 		$('#boardBox').css({"margin":"0px"})
 	}
 	else {
-		// console.log("spaceWidth is "+spaceWidth)
-		// console.log("maxWidth is "+maxWidth)
 		var paddingWidth = (spaceWidth - maxWidth)/2;
-		// console.log("Changing padding to: " + paddingWidth.toString());
 		$('#boardBox').css({"padding-left":paddingWidth.toString()+"px"});
 		$('#boardBox').css({"padding-right":paddingWidth.toString()+"px"});
 	}
@@ -44,7 +28,24 @@ function resizeBoard() {
 	$('#knightourBoard td').height($('#theBoard').width()/boardSize);
 }
 
-$(document).ready(function () {
-	resizeBoard();
-	$(window).resize(resizeBoard);
+$(document).ready(function() {
+	// This fills the gap left when the navbar becomes "sticky"
+	$('#nav-wrapper').height($('#nav').height()+21);
 })
+
+$(document).ready(function () {
+	console.log("longtitle")
+	var longTitle = ($('#title').text().length > 8);
+	if (longTitle) {
+		checkWidth();
+	}
+})
+
+$(window).resize(checkWidth);
+
+$(document).ready(function () {
+	console.log("resize board")
+	resizeBoard();
+})
+
+$(window).resize(resizeBoard);
